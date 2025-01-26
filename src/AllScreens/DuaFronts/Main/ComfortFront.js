083c46cc1229db02cbb439d0525cd24ada029f91
Mudz1212 <149740018+Mudz1212@users.noreply.github.com>
2025-01-26 23:59:47 +0000

    Added another screen
    Another comfort back screen with drop downs however still using dummy data

--- src/AllScreens/DuaFronts/Main/ComfortFront.js
+++ src/AllScreens/DuaFronts/Main/ComfortFront.js
@@ -2,14 +2,14 @@ import React from "react";
 import DuaFront from "../../../Layout/DuaFront";
 
 const Comfortdata = [
-  { title: "Al Baqarah", description: "The greatest protection" },
-  { title: "Surah Al-Fatihah", description: "The Opening of the Quran" },
-  { title: "Surah Al-Ikhlas", description: "The essence of monotheism" },
+  { id: 1, title: "Al Baqarah", description: "The greatest protection" },
+  { id: 2, title: "Surah Al-Fatihah", description: "The Opening of the Quran" },
+  { id: 3, title: "Surah Al-Ikhlas", description: "The essence of monotheism" },
 ];
 
 export const ComfortFront = ({ navigation }) => {
   const handleItemPress = (item) => {
-    console.log("Item pressed:", item); // Will have this navigate to something soon
+    navigation.navigate("ComfortBack", { item });
   };
 
   return (
