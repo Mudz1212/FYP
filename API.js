312f9878dd8d91f0cc578ae09c90208ec6e49757
Mudz1212 <149740018+Mudz1212@users.noreply.github.com>
2025-02-07 23:51:26 +0000

    Trying to solve the API issue
    Trying to get the data from firebase however the API set up is hard.

--- API.js
+++ API.js
@@ -1,14 +1,14 @@
 const API = {};
 
-API.get = (endpoint) => callFetch(`${endpoint}`, "GET");
-API.post = (endpoint, data) => callFetch(`${endpoint}`, "POST", data);
-API.put = (endpoint, data) => callFetch(`${endpoint}`, "PUT", data);
-API.delete = (endpoint) => callFetch(`${endpoint}`, "DELETE");
+const BASE_URL = "https://EchoesOfPeace.cloudfunctions.net";
 
-export default API;
+API.getDuas = () => callFetch(`${BASE_URL}/duas`, "GET");
+
+module.exports = API;
 
 const callFetch = async (endpoint, method, dataObj = null) => {
   let requestObj = { method: method };
+
   if (dataObj)
     requestObj = {
       ...requestObj,
